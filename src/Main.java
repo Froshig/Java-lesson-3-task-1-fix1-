2d456f6f5a471d1bf90f61f5fe2be4fd12bd6ba4
Maxim <froshig@gmail.com>
2022-11-26 19:16:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketsPrice = 5000;
+        int costPerBonus = 20;
+        int bonus = ticketsPrice / costPerBonus;
+        System.out.println(bonus + " миль");
+    }
+}
